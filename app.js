27ebda030496091869daba5853109326efa68af0
mobak88 <mostafa0401@ymail.com>
2022-09-14 20:16:27 +0200

    Setting up app.js with express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
+app.listen(port, () => {
+    console.log("Server started on port", port);
+});
