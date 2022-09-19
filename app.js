9508b4a5a55d8dd00f2b9035a1b1a7761a5ea7fe
mobak88 <mostafa0401@ymail.com>
2022-09-19 11:09:10 +0200

    Creating form, importing script to html, installing body parser

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/user', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/user.html'));
 });
 
 app.listen(port, () => {
---
+++ public/scripts/index.js
@@ -0,0 +1,6 @@
+const loginBtn = document.querySelector('.login-btn');
+
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Test');
+});
